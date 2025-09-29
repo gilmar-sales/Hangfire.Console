c947219d92465a32d0a600b3bbc3b562b2b174d5
Gilmar Sales <gilmar.custodio@outlook.com.br>
2025-09-29 17:51:05 -0300

    fix: Add support to PrefixPath on DynamicJsDispatcher.cs

--- src/Hangfire.Console/Dashboard/DynamicJsDispatcher.cs
+++ src/Hangfire.Console/Dashboard/DynamicJsDispatcher.cs
@@ -20,11 +20,13 @@ internal class DynamicJsDispatcher : IDashboardDispatcher
     public Task Dispatch(DashboardContext context)
     {
         var builder = new StringBuilder();
+        
+        var consolePollUrl = Path.Combine(context.Options.PrefixPath, context.Request.PathBase, "console/");
 
         builder.Append("(function (hangfire) {")
             .Append("hangfire.config = hangfire.config || {};")
             .AppendFormat("hangfire.config.consolePollInterval = {0};", _options.PollInterval)
-            .AppendFormat("hangfire.config.consolePollUrl = '{0}/console/';", context.Request.PathBase)
+            .AppendFormat("hangfire.config.consolePollUrl = '{0}';", consolePollUrl)
             .Append("})(window.Hangfire = window.Hangfire || {});")
             .AppendLine();
 
